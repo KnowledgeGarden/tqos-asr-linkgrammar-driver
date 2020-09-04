f6316bbed959b2bc4cdaa1e1ebf3949d6db2c3b8
Jack Park <jackpark@gmail.com>
2020-09-04 11:49:31 -0700

    first commit

---
+++ src/test/java/devtests/TestRoot.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package devtests;
+
+import org.topicquests.os.asr.driver.LGDriverEnvironment;
+
+/**
+ * @author jackpark
+ *
+ */
+public class TestRoot {
+	protected LGDriverEnvironment environment;
+
+	/**
+	 * 
+	 */
+	public TestRoot() {
+		environment = new LGDriverEnvironment();
+	}
+
+}
